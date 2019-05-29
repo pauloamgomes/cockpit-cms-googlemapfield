b8ccb004dab8e9cdafcba9c1153c7531cb4d51be
Paulo Gomes <pauloamgomes@gmail.com>
2019-05-29 22:42:11 +0100

    regex update, small refactoring

--- bootstrap.php
+++ bootstrap.php
@@ -5,15 +5,13 @@
  * Cockpit module bootstrap implementation.
  */
 
-$this->module('googlemapsfield')->extend([
-
-  'getConfig' => function() : array {
-    return $this->app->config['google']['maps'] ?? [];
-  },
-
-]);
-
 // Include admin.
 if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
+  $this->module('googlemapsfield')->extend([
+    'getConfig' => function() : array {
+      return $this->app->config['google']['maps'] ?? [];
+    },
+  ]);
+
   include_once __DIR__ . '/admin.php';
 }
